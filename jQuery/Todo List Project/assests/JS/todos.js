6b5f9fd30fd8ec0d8c1d80d1f0a2ac5439d5ff63
Anik <marufmonem@gmail.com>
2020-05-31 16:30:12 +0600

    Added the adding and delete functionality.

--- jQuery/Todo List Project/assests/JS/todos.js
+++ jQuery/Todo List Project/assests/JS/todos.js
@@ -1,1 +1,30 @@
 // alert("connected");
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+// for x button
+$("ul").on("click", "span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stoppropagation();
+})
+
+// Handling the user input
+$("input[type='text']").keypress(function(event){
+    // event reponds to the current keypress event
+    //checking for enter key
+    if(event.which === 13){
+
+        //if the user presses enter get the value in the input
+        var userValue= $(this).val();
+
+        //add the input value to the unordered list
+        $("ul").append("<li><span>X </span>" + userValue + "</li>");
+
+        // Clear out the input space
+        $(this).val("");
+    }
+});
+    
